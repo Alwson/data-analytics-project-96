947578de5b2a20831de63857971061b30f69d5e0
Alwson <leg.alien@yandex.ru>
2025-08-30 22:48:37 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -9,7 +9,7 @@ with tab as (
         l.amount,
         l.closing_reason,
         l.status_id,
-        -- затем вычисляемые
+        -- вычисляемые
         s.visit_date::timestamp as visit_ts,
         s.visit_date::date as visit_date,
         lower(s.source) as utm_source,
@@ -19,11 +19,14 @@ with tab as (
             partition by s.visitor_id
             order by s.visit_date desc, s.visitor_id desc
         ) as rn
-    from sessions as s
-    left join leads as l
+    from
+        sessions as s
+    left join
+        leads as l
         on s.visitor_id = l.visitor_id
-        and s.visit_date <= coalesce(l.created_at, s.visit_date)  -- AM05: условие в ON
-    where lower(s.medium) <> 'organic'
+        and s.visit_date <= coalesce(l.created_at, s.visit_date)
+    where
+        lower(s.medium) <> 'organic'
 ),
 
 last_paid_click as (
@@ -36,8 +39,10 @@ last_paid_click as (
         count(t.lead_id) as leads_count,
         count(*) filter (where t.status_id = 142) as purchases_count,
         sum(t.amount) as revenue
-    from tab as t
-    where t.rn = 1
+    from
+        tab as t
+    where
+        t.rn = 1
     group by
         t.visit_date,
         t.utm_source,
@@ -59,45 +64,10 @@ ads as (
             utm_medium,
             utm_campaign,
             daily_spent
-        from vk_ads
+        from
+            vk_ads
         union all
         select
             campaign_date,
             utm_source,
             utm_medium,
-            utm_campaign,
-            daily_spent
-        from ya_ads
-    ) as a
-    group by
-        a.campaign_date::date,
-        lower(a.utm_source),
-        lower(a.utm_medium),
-        lower(a.utm_campaign)
-)
-
-select
-    lpv.visit_date,
-    lpv.visitors_count,
-    lpv.utm_source,
-    lpv.utm_medium,
-    lpv.utm_campaign,
-    a.total_cost,
-    lpv.leads_count,
-    lpv.purchases_count,
-    lpv.revenue
-from last_paid_click as lpv
-left join ads as a
-    on lpv.visit_date = a.campaign_date
-    and lpv.utm_source = a.utm_source
-    and lpv.utm_medium = a.utm_medium
-    and lpv.utm_campaign = a.utm_campaign
-order by
-    lpv.visit_date asc,
-    lpv.visitors_count desc,
-    lpv.utm_source asc,
-    lpv.utm_medium asc,
-    lpv.utm_campaign asc,
-    lpv.revenue desc nulls last
-limit 15;
-
