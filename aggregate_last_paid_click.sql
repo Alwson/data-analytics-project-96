45e62db531ff558d6670182b084b2c24aad99248
Alwson <leg.alien@yandex.ru>
2025-08-30 22:39:26 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -2,17 +2,19 @@
 
 with tab as (
     select
+        -- ST06: простые поля
         s.visitor_id,
-        s.visit_date::timestamp as visit_ts,
-        s.visit_date::date as visit_date,
-        lower(s.source) as utm_source,
-        lower(s.medium) as utm_medium,
-        lower(s.campaign) as utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id,
+        -- затем вычисляемые
+        s.visit_date::timestamp as visit_ts,
+        s.visit_date::date as visit_date,
+        lower(s.source) as utm_source,
+        lower(s.medium) as utm_medium,
+        lower(s.campaign) as utm_campaign,
         row_number() over (
             partition by s.visitor_id
             order by s.visit_date desc, s.visitor_id desc
@@ -20,7 +22,7 @@ with tab as (
     from sessions as s
     left join leads as l
         on s.visitor_id = l.visitor_id
-        and s.visit_date <= l.created_at
+        and s.visit_date <= coalesce(l.created_at, s.visit_date)  -- AM05: условие в ON
     where lower(s.medium) <> 'organic'
 ),
 
@@ -32,9 +34,7 @@ last_paid_click as (
         t.utm_campaign,
         count(t.visitor_id) as visitors_count,
         count(t.lead_id) as leads_count,
-        count(*) filter (
-            where t.status_id = 142
-        ) as purchases_count,
+        count(*) filter (where t.status_id = 142) as purchases_count,
         sum(t.amount) as revenue
     from tab as t
     where t.rn = 1
