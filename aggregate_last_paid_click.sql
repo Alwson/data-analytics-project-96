4e6b02d6cc5a118263e225d66917ac8d64e8da5a
Alwson <leg.alien@yandex.ru>
2025-08-30 22:21:06 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,3 +1,5 @@
+-- aggregate_last_paid_click.sql
+
 with tab as (
     select
         s.visitor_id,
@@ -31,4 +33,69 @@ last_paid_click as (
         count(t.visitor_id) as visitors_count,
         count(t.lead_id) as leads_count,
         count(*) filter (where t.status_id = 142) as purchases_count,
-        sum(t.amount) as reven
+        sum(t.amount) as revenue
+    from tab as t
+    where t.rn = 1
+    group by
+        t.visit_date,
+        t.utm_source,
+        t.utm_medium,
+        t.utm_campaign
+),
+
+ads as (
+    select
+        a.campaign_date::date as campaign_date,
+        lower(a.utm_source) as utm_source,
+        lower(a.utm_medium) as utm_medium,
+        lower(a.utm_campaign) as utm_campaign,
+        sum(a.daily_spent) as total_cost
+    from (
+        select
+            campaign_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        from vk_ads
+        union all
+        select
+            campaign_date,
+            utm_source,
+            utm_medium,
+            utm_campaign,
+            daily_spent
+        from ya_ads
+    ) as a
+    group by
+        a.campaign_date::date,
+        lower(a.utm_source),
+        lower(a.utm_medium),
+        lower(a.utm_campaign)
+)
+
+select
+    lpv.visit_date,
+    lpv.visitors_count,
+    lpv.utm_source,
+    lpv.utm_medium,
+    lpv.utm_campaign,
+    a.total_cost,
+    lpv.leads_count,
+    lpv.purchases_count,
+    lpv.revenue
+from last_paid_click as lpv
+left join ads as a
+    on lpv.visit_date = a.campaign_date
+    and lpv.utm_source = a.utm_source
+    and lpv.utm_medium = a.utm_medium
+    and lpv.utm_campaign = a.utm_campaign
+order by
+    lpv.visit_date asc,
+    lpv.visitors_count desc,
+    lpv.utm_source asc,
+    lpv.utm_medium asc,
+    lpv.utm_campaign asc,
+    lpv.revenue desc nulls last
+limit 15;
+
